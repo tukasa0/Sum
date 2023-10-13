5f93ea857ba63e8f804771696d9034788f26872f
sk2k0 <sk2k0@MSI>
2023-10-13 11:50:08 +0900

    プロジェクト ファイルを追加します。

---
+++ Sum.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int Sum(int value1, int value2)
+{
+	return value1 + value2;
+}
---
+++ 教科書確認問題2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int Sum(int value1, int value2);
+
+int main()
+{
+	int value1 = 2;
+	int value2 = 3;
+
+	cout << Sum(value1, value2) << endl;
+}
